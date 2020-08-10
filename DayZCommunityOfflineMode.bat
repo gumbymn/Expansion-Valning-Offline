0615afed2a1e42b19fb595813f73b097c8a651ed
gumbymn <64388141+gumbymn@users.noreply.github.com>
2020-08-10 15:25:44 -0500

    Updating path

--- DayZCommunityOfflineMode.bat
+++ DayZCommunityOfflineMode.bat
@@ -6,4 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.Valning -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@DayZ-Expansion-Licensed;!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@BuilderItems;!Workshop\@Valning Map;"
+start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.Valning -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@DayZ-Expansion-Licensed;!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@BuilderStatics;!Workshop\@Valning Map;"
