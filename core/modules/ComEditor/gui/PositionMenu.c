e50e75b2c68fcc997d30aea5e947398442ed02cd
gumbymn <64388141+gumbymn@users.noreply.github.com>
2020-08-10 14:19:08 -0500

    Adding teleport locations to Offline Mission

--- core/modules/ComEditor/gui/PositionMenu.c
+++ core/modules/ComEditor/gui/PositionMenu.c
@@ -16,7 +16,37 @@ class COM_PositionMenu extends PopupMenu
 	{
 		m_bOverCurrentPos = false;
 
-		// Positions.Insert( "name", "pos" );
+		Positions.Insert( "Kemen Airstrip", "4061 0 8935" );
+        Positions.Insert( "Shauninka", "3299 0 8095" );
+        Positions.Insert( "Fersen Prison", "2875 0 7363" );
+        Positions.Insert( "Noradeshko", "3783 0 7888" );
+        Positions.Insert( "Kalemka", "6058 0 7417" );
+        Positions.Insert( "Valning Theme Park", "5128 0 6639" );
+        Positions.Insert( "Paulaner", "4114 0 6942" );
+        Positions.Insert( "Kerl", "4555 0 6450" );
+        Positions.Insert( "Jagger Vault", "8067 0 5741" );
+        Positions.Insert( "Famka", "7367 0 5675" );
+        Positions.Insert( "Guerstling", "5485 0 5187" );
+        Positions.Insert( "Otha Island", "3706 0 5756" );
+        Positions.Insert( "Blinka", "2435 0 5595" );
+        Positions.Insert( "Vezga", "3973 0 4761" );
+        Positions.Insert( "Eshkoe Military", "6180 0 4207" );
+        Positions.Insert( "Eshkoe", "6124 0 3811" );
+        Positions.Insert( "Tervia", "5207 0 3503" );
+        Positions.Insert( "Vishky Mountain", "7847 0 4359" );
+        Positions.Insert( "Smokinki", "8246 0 4833" );
+        Positions.Insert( "Winaud Castle", "4114 0 3642" );
+        Positions.Insert( "Gresovia", "3454 0 3657" );
+        Positions.Insert( "Kreozky", "2962 0 2943" );
+        Positions.Insert( "Loebek", "2794 0 1695" );
+        Positions.Insert( "Seroe", "3476 0 1487" );
+        Positions.Insert( "Airforce Base", "2946 0 714" );
+        Positions.Insert( "Oshkey Radio", "7009 0 1257" );
+        Positions.Insert( "Vezerovsky", "7131 0 2659" );
+        Positions.Insert( "Hansking", "7839 0 1691" );
+        Positions.Insert( "Kaelka", "8355 0 651" );
+        Positions.Insert( "Clear Military", "8808 0 1251" );
+        Positions.Insert( "Blackwater Castle", "8393 0 1799" );
 
 	}
 
