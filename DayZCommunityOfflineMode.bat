ffbbc8c519c1398854e554a173c42c526a2294ba
gumbymn <64388141+gumbymn@users.noreply.github.com>
2020-08-10 15:39:44 -0500

    Update for bat launch parameters.

--- DayZCommunityOfflineMode.bat
+++ DayZCommunityOfflineMode.bat
@@ -6,8 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-<<<<<<< HEAD
 start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.Valning -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@DayZ-Expansion-Licensed;!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@BuilderStatics;!Workshop\@Valning Map;"
-=======
-start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.Valning -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion;!Workshop\@DayZ-Expansion-Licensed;!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@BuilderItems;!Workshop\@Valning Map;"
->>>>>>> 3f9d4ddc7cfcea21481e91c25f7b67301d0d9edb
